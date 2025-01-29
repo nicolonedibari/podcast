7957e92f3151fef1036b20eea56b97ec80de6733
nicolonedibari <162788540+nicolonedibari@users.noreply.github.com>
2025-01-29 10:59:28 +0100

    avanzamento

--- static/js/audio.js
+++ static/js/audio.js
@@ -1,7 +1,18 @@
 let Mate = new Audio("static/audio/gay.mp3");
 
-
 Mate.addEventListener("canplaythrough", (event) => {
     Mate.play();
+});
+
+let Ita = new Audio("static/audio/gay.mp3");
+
+Ita.addEventListener("canplaythrough", (event) => {
+    Ita.play();
+});
+
+let Sis = new Audio("static/audio/gay.mp3");
+
+Sis.addEventListener("canplaythrough", (event) => {
+    Sis.play();
 });
  
